d62629bda59fbb2146fe909b46ffbd1577c6bce2
Austin Chiatto <chiattoaustin@gmail.com>
2023-04-05 12:00:10 -0700

    continued work on grid feature

--- public/interplanetary.php
+++ public/interplanetary.php
@@ -103,29 +103,29 @@ require("../src/partials/shared/base/head.php");
                             </ul>
                             <div class="o-process-container js-process-container">
                                 <h2 class="o-process__heading">Research</h2>
-                                <p class="u-font-highlight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati non vero, voluptate quod, et ab minus ducimus nesciunt, blanditiis maxime vel error pariatur laudantium.</p>
+                                <p class="u-font-highlight o-process__copy">As this project involved providing real-time launch dates and information, it was necessary to utilize an API. However, as I had no prior experience with REST APIs, I dedicated the initial phase of the project to conducting extensive research on their requirements and identifying the necessary resources to obtain the live information required.</p>
                             </div>
                             <div class="o-process-container js-process-container">
                                 <h2 class="o-process__heading">Planning</h2>
-                                <p class="u-font-highlight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati non vero, voluptate quod, et ab minus ducimus nesciunt, blanditiis maxime vel error pariatur laudantium.</p>
+                                <p class="u-font-highlight o-process__copy">Since this was a large project, I wanted to make sure I had a solid system in place for organizing files and naming conventions. To achieve this, I decided to use a combination of BEM and Atomic Design Language as a framework for naming files and elements. This approach made it much easier to navigate, manage, and document the project as it developed over time.</p>
                             </div>
                             <div class="o-process-container js-process-container">
                                 <h2 class="o-process__heading">Design</h2>
-                                <p class="u-font-highlight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati non vero, voluptate quod, et ab minus ducimus nesciunt, blanditiis maxime vel error pariatur laudantium.</p>
+                                <p class="u-font-highlight o-process__copy">As I followed a modular approach for naming conventions and organizational structure, which I also applied to the UI design. Consistent section styling and a grid-based system for the launch cards allowed for dynamic and flexible display of launch data, ensuring modularity and scalability throughout development.</p>
                             </div>
                             <div class="o-process-container js-process-container">
                                 <h2 class="o-process__heading">Development</h2>
-                                <p class="u-font-highlight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati non vero, voluptate quod, et ab minus ducimus nesciunt, blanditiis maxime vel error pariatur laudantium.</p>
+                                <p class="u-font-highlight o-process__copy">By prioritizing modularity and scalability in the design and planning phases throughout development, I was able to maintain a high level of flexibility and adaptability to changes in project requirements.</p>
                             </div>
                         </div>
                     </li>
                     <li class="o-project-details__card--2x1">
                         <article class="m-launch-card">
                             <div class="m-launch-card__details">
-                                <div class="m-launch-card__mission">Starlink Group 2-5</div>
-                                <div class="m-launch-card__vehicle">Faclon 9 Block 5</div>
+                                <div class="m-launch-card__mission">Starlink Group 2</div>
+                                <div class="m-launch-card__vehicle">Faclon 9</div>
                                 <div class="m-launch-card__provider">SpaceX</div>
-                                <div class="m-launch-card__date">Mar 12</div>
+                                <div class="m-launch-card__date">April 8</div>
                             </div>
                             <picture class="m-launch-card__picture">
                                 <img class="m-launch-card__img" src="../src/assets/images/rocket.jpg" alt="A white, frost covered, falcon 9 rocket that carries starlink satellites, launching from earth during dusk." />
@@ -137,8 +137,8 @@ require("../src/partials/shared/base/head.php");
                             <h3 class="o-project-details__heading">Launch Controls</h3>
                             <h4 class="o-project-details__heading--sub">Get a New Mission</h4>
                             <ul class="m-launch-control__btns">
-                                <li class="m-launch-control__btn"><img src="../src/assets/icons/arrowtriangle-left-fill.svg" alt=""></li>
-                                <li class="m-launch-control__btn"><img src="../src/assets/icons/arrowtriangle-right-fill.svg" alt=""></li>
+                                <li class="m-launch-control__btn"><img src="../src/assets/icons/arrowtriangle-left-fill.svg" alt="left arrow key button that controls the launch card api to get a previous launch"></li>
+                                <li class="m-launch-control__btn"><img src="../src/assets/icons/arrowtriangle-right-fill.svg" alt="right arrow key button that controls the launch card api to get the next launch"></li>
                             </ul>
                         </div>
                     </li>
@@ -193,9 +193,7 @@ fetch(previousLaunchesURI)
                     </li>
                     <li class="o-project-details__card">
                         <div class="m-details-mockup">
-                            <picture class="m-details-mockup__container">
-                                <img class="m-details-mockup__img" src="../src/assets/images/interplanetary-mockup__mobile-01.png" alt="A white, frost covered, falcon 9 rocket that carries starlink satellites, launching from earth during dusk." />
-                            </picture>
+                            <p class="a-heading">🚀</p>
                         </div>
                     </li>
                     <li class="o-project-details__card--2x1">
@@ -228,7 +226,17 @@ function fetchUpcomingLaunches() {
                         </div>
                     </li>
                     <li class="o-project-details__card--2x1">
-                    
+                        <article class="m-launch-card">
+                            <div class="m-launch-card__details">
+                                <div class="m-launch-card__mission">OFT Test Launch</div>
+                                <div class="m-launch-card__vehicle">Starship</div>
+                                <div class="m-launch-card__provider">SpaceX</div>
+                                <div class="m-launch-card__date">April 10</div>
+                            </div>
+                            <picture class="m-launch-card__picture">
+                                <img class="m-launch-card__img" src="../src/assets/images/rocket.jpg" alt="A white, frost covered, falcon 9 rocket that carries starlink satellites, launching from earth during dusk." />
+                            </picture>
+                        </article>
                     </li>
                     
                     <li class="o-project-details__card--2x2">
@@ -236,27 +244,33 @@ function fetchUpcomingLaunches() {
                         <ul class="o-process__tab-list">
                                 <li class="o-process__tab"><a class="a-button--invert js-process-btn">Challenges</a></li>
                                 <li class="o-process__tab"><a class="a-button--invert js-process-btn">Solutions</a></li>
-                                <li class="o-process__tab"><a class="a-button--invert js-process-btn">What I learned</a></li>
+                                <li class="o-process__tab"><a class="a-button--invert js-process-btn">What I would change</a></li>
                             </ul>
                             <div class="o-process-container js-process-container">
-                                <h2 class="o-process__heading">Research</h2>
-                                <p class="u-font-highlight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati non vero, voluptate quod, et ab minus ducimus nesciunt, blanditiis maxime vel error pariatur laudantium.</p>
+                                <h2 class="o-process__heading">Challenges</h2>
+                                <p class="u-font-highlight o-process__copy">The most difficult part of this project was writing the code for the search function in order to find specific launch information based on the search query. I found passing that data to a new GET request dynamically and showing the new set of launch cards the most difficult part of the feature.</p>
                             </div>
                             <div class="o-process-container js-process-container">
-                                <h2 class="o-process__heading">Planning</h2>
-                                <p class="u-font-highlight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati non vero, voluptate quod, et ab minus ducimus nesciunt, blanditiis maxime vel error pariatur laudantium.</p>
+                                <h2 class="o-process__heading">Solutions</h2>
+                                <p class="u-font-highlight o-process__copy">I found that if I ran the query every few seconds as long as the search field was active, then removing all of the cards in the container, and finally re-appending the new cards captured from the search query the easiest way to overcome the challenge.</p>
                             </div>
                             <div class="o-process-container js-process-container">
-                                <h2 class="o-process__heading">Design</h2>
-                                <p class="u-font-highlight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati non vero, voluptate quod, et ab minus ducimus nesciunt, blanditiis maxime vel error pariatur laudantium.</p>
+                                <h2 class="o-process__heading">What I Would Change</h2>
+                                <p class="u-font-highlight o-process__copy">In this project, I learned a lot and realized that I could have optimized the API requests for improved efficiency. Instead of writing separate requests and functions for each call, I could have consolidated them into fewer requests to streamline the process and improve overall performance.</p>
                             </div>
                         </div>
                     </li>
                     <li class="o-project-details__card">
-                    
+                        <div class="m-details-mockup">
+                            <picture class="m-details-mockup__container">
+                                <img class="m-details-mockup__img" src="../src/assets/images/interplanetary-mockup__mobile-02.png" alt="A white, frost covered, falcon 9 rocket that carries starlink satellites, launching from earth during dusk." />
+                            </picture>
+                        </div>
                     </li>
                     <li class="o-project-details__card">
-                    
+                        <div class="m-details-mockup">
+                            <p class="a-heading">🪐</p>
+                        </div>
                     </li>
                 </ul>
             </section>
