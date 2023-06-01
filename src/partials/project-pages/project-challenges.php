bc8db3d14a61ed991d6eb405149788d7f1345dac
Austin Chiatto <chiattoaustin@gmail.com>
2023-06-01 09:37:01 -0700

    continued work on layout

---
+++ src/partials/project-pages/project-challenges.php
@@ -0,0 +1,40 @@
+<section class="s-section">
+    <div class="s-section__content">
+        <div class="m-intro-container m-intro-container--vh100 js-chalHeaderCont">
+            <h2 class="a-text--center js-chalHeader">Challenges Overcome <span class="a-text--highlight">in the</span> Development <span class="a-text--highlight">of [project name]</span></h2>
+        </div>
+    </div>
+</section>
+
+<div class="o-challenge">
+    <!-- <section class="s-section--full-height ">
+        <div class="o-challenge__header ">
+            <h2 class="a-text--center">Challenges Overcome <span class="a-text--highlight">in the</span> Development <span class="a-text--highlight">of [project name]</span></h2>
+        </div>
+    </section> -->
+    <section class="s-section js-chalImgCont">
+        <div class="s-section__content o-challenge__img-cont js-chalImg">
+            <img class="o-challenge__img" src="../src/assets/images/test-challenge-mockup.png" alt="">
+        </div>
+    </section>
+    <section class="s-section js-chalCopyCont">
+        <div class="s-section__content--wide o-challenge__grid">
+            <div class="o-challenge__copy">
+                <h3>Challenge</h3>
+                <p class="a-text--highlight">description Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora alias ad quod fuga error! Rem?</p>
+            </div>
+            <div class="o-challenge__copy">
+                <h3>Solution</h3>
+                <p class="a-text--highlight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora alias ad quod fuga error! Rem?</p>
+            </div>
+            <div class="o-challenge__copy">
+                <h3>Challenge</h3>
+                <p class="a-text--highlight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora alias ad quod fuga error! Rem?</p>
+            </div>
+            <div class="o-challenge__copy js-chalCopyTrig">
+                <h3>Solution</h3>
+                <p class="a-text--highlight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora alias ad quod fuga error! Rem?</p>
+            </div>
+        </div>
+    </section>
+</div>
---
+++ src/partials/project-pages/project-hero.php
@@ -0,0 +1,13 @@
+<section class="s-section">
+    <div class="s-section__content s-section__content--full o-project-hero js-projHero">
+        <!-- hero headings -->
+        <div class="m-intro-container m-intro-container--vh100">
+            <h1>Interplanetary</h1>
+            <h2 class="a-text--sm">Past & Future Launch Tracking</h2>
+        </div>
+        <!-- hero animated mockup -->
+        <div class="o-project-hero__img-container js-projHeroImgCont">
+            <img class="o-project-hero__img js-projHeroImg" src="../src/assets/images/testDisplay.png" alt="">
+        </div>
+    </div>
+</section>
