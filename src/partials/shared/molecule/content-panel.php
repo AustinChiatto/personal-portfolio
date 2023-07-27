1d78701c2fd3379866c3abfd47c9aea1fda33ecb
Austin Chiatto <chiattoaustin@gmail.com>
2023-07-27 13:52:29 -0700

    fixed bug with panel close button

--- src/partials/shared/molecule/content-panel.php
+++ src/partials/shared/molecule/content-panel.php
@@ -1,9 +1,10 @@
 <div class="m-content-panel js-contentPanel">
+    <div class="m-content-panel__close-icon js-panelClose">
+        <img src="src/assets/icons/Close.svg" width="24" height="24" alt="close icon">
+    </div>
     <div class="m-content-panel__container">
         <aside data-lenis-prevent class="m-content-panel__modal js-panelModal">
-            <div class="m-content-panel__close-icon js-panelClose">
-                <img src="src/assets/icons/Close.svg" width="24" height="24" alt="close icon">
-            </div>
+            
             <div class="js-panelContent">
                 <?php isset($_GET["cardId"]) ? require "./panel-content.php" : "error loading content"; ?>
             </div>
