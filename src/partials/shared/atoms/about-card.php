022a403c6dfb0be6bdcfcee268589cb544e9a357
Austin Chiatto <chiattoaustin@gmail.com>
2023-07-27 22:56:07 -0700

    removed titles from cards

--- src/partials/shared/atoms/about-card.php
+++ src/partials/shared/atoms/about-card.php
@@ -1,5 +1,5 @@
 <?php if ($detail->external_url): ?>
-    <a id="<?= $detail->type ?>" class="a-card<?= $detail->card_type ? $detail->card_type : null ?> js-projectDetailCard <?= !$detail->panel_content ? "js-disablePanel a-card-inactive" : null ?>" title="<?= $detail->pre_heading ?>">
+    <a id="<?= $detail->type ?>" class="a-card<?= $detail->card_type ? $detail->card_type : null ?> js-projectDetailCard <?= !$detail->panel_content ? "js-disablePanel a-card-inactive" : null ?>">
         <?php if ($detail->pre_heading): ?>
             <div class="a-card__headings">
                 <h4 class="a-text--base a-text--highlight"><?= $detail->pre_heading ?></h4>
@@ -9,7 +9,7 @@
         <?= $detail->card_content ?>
     </a>
 <?php else: ?>
-    <div id="<?= $detail->type ?>" class="a-card<?= $detail->card_type ? $detail->card_type : null ?> js-projectDetailCard <?= !$detail->panel_content ? "js-disablePanel a-card-inactive" : null ?>" title="<?= $detail->pre_heading ?>">
+    <div id="<?= $detail->type ?>" class="a-card<?= $detail->card_type ? $detail->card_type : null ?> js-projectDetailCard <?= !$detail->panel_content ? "js-disablePanel a-card-inactive" : null ?>">
         <?php if ($detail->pre_heading): ?>
             <div class="a-card__headings">
                 <h4 class="a-text--base a-text--highlight"><?= $detail->pre_heading ?></h4>
--- src/partials/shared/atoms/detail-card.php
+++ src/partials/shared/atoms/detail-card.php
@@ -1,4 +1,4 @@
-<a id="<?= $detail->type ?>" class="a-card<?= $page_type == "project-detail" ? $detail->card_type : null ?> js-projectDetailCard" href="<?= $detail->external_url ? $detail->external_url : null ?>" title="<?= $detail->heading ?>">
+<a id="<?= $detail->type ?>" class="a-card<?= $page_type == "project-detail" ? $detail->card_type : null ?> js-projectDetailCard" href="<?= $detail->external_url ? $detail->external_url : null ?>">
     <div class="a-card__headings">
         <h4 class="a-text--base a-text--highlight"><?= $detail->pre_heading ?></h4>
         <h3><?= $detail->heading ?></h3>
